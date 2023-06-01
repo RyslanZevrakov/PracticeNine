7ce85fda779f215178034e7e82b2e81562a1f0c0
zhevrakov.ra <zhevrakov.ra.ki25@mail.ru>
2023-06-01 14:39:36 +0300

    jj

--- src/task_two/Hotel.java
+++ src/task_two/Hotel.java
@@ -1,53 +1,44 @@
 package task_two;
 
-public class Hotel { public static int getRandomDiceNumber()
-{
-    return (int) (Math.random() * 10) + 1;
-}
-    public static int getRandomNumber()
-    {
+public class Hotel {
+    public static int getRandomDiceNumber() {
+        return (int) (Math.random() * 10) + 1;
+    }
+
+    public static int getRandomNumber() {
         return (int) (Math.random() * 3) + 0;
     }
 
     public void hotel() {
-       int numder = getRandomDiceNumber();
-        if (numder==1){
-            double numder1 =getRandomNumber();
-            System.out.println("Лайтхаус " + "Количество звёзд "+numder1 +" класс номера:однокомнатный");
-        }
-        else if (numder==2){
+        int numder = getRandomDiceNumber();
+        if (numder == 1) {
+            double numder1 = getRandomNumber();
+            System.out.println("Лайтхаус " + "Количество звёзд " + numder1 + " класс номера:однокомнатный");
+        } else if (numder == 2) {
             double numder2 = getRandomNumber();
-            System.out.println("Ritz Plaza " + "Количество звёзд "+numder2 +" класс номера:двухкомнатный");
-        }
-        else if (numder==3){
+            System.out.println("Ritz Plaza " + "Количество звёзд " + numder2 + " класс номера:двухкомнатный");
+        } else if (numder == 3) {
             double numder3 = getRandomNumber();
-            System.out.println("Мини-отель " + "Количество звёзд "+numder3 +" класс номера:люкс");
-        }
-        else if (numder==4){
+            System.out.println("Мини-отель " + "Количество звёзд " + numder3 + " класс номера:люкс");
+        } else if (numder == 4) {
             double numder4 = getRandomNumber();
-            System.out.println("AvHouse " + "Количество звёзд "+numder4 +" класс номера:президентский");
-        }
-        else if (numder==5){
+            System.out.println("AvHouse " + "Количество звёзд " + numder4 + " класс номера:президентский");
+        } else if (numder == 5) {
             double numder5 = getRandomNumber();
-            System.out.println("РумОтель " + "Количество звёзд "+numder5 +" класс номера:однокомнатный");
-        }
-        else if (numder==6){
+            System.out.println("РумОтель " + "Количество звёзд " + numder5 + " класс номера:однокомнатный");
+        } else if (numder == 6) {
             double numder6 = getRandomNumber();
-            System.out.println("Coziness " + "Количество звёзд "+numder6 +" класс номера:двухкомнатный");
-        }
-        else if (numder==7){
+            System.out.println("Coziness " + "Количество звёзд " + numder6 + " класс номера:двухкомнатный");
+        } else if (numder == 7) {
             double numder7 = getRandomNumber();
-            System.out.println("Спектр-Отель " + "Количество звёзд "+numder7 +" класс номера:люкс");
-        }
-        else if (numder==8){
+            System.out.println("Спектр-Отель " + "Количество звёзд " + numder7 + " класс номера:люкс");
+        } else if (numder == 8) {
             double numder8 = getRandomNumber();
-            System.out.println("O'Hostel " + "Количество звёзд "+numder8 +" класс номера:президентский");
-        }
-        else if (numder==9){
+            System.out.println("O'Hostel " + "Количество звёзд " + numder8 + " класс номера:президентский");
+        } else if (numder == 9) {
             double numder9 = getRandomNumber();
-            System.out.println("Гельвеция " + "Количество звёзд "+numder9 +" класс номера:двухкомнатный");
-        }
-        else {
+            System.out.println("Гельвеция " + "Количество звёзд " + numder9 + " класс номера:двухкомнатный");
+        } else {
             System.out.println("Ищите жильё сами");
         }
     }
--- src/task_two/Tour.java
+++ src/task_two/Tour.java
@@ -4,51 +4,42 @@ import java.util.Scanner;
 
 public class Tour {
     AirTicket airTicket = new AirTicket();
-    public static int getRandomDiceNumber()
-    {
+
+    public static int getRandomDiceNumber() {
         return (int) (Math.random() * 10) + 1;
     }
-   public void transition() {
-       int numder = getRandomDiceNumber();
-if (numder==1){
-airTicket.aVoid();
 
-}
-else if (numder==2){
-    airTicket.aVoid();
-}
-else if (numder==3){
-    airTicket.aVoid();
-}
-else if (numder==4){
-    airTicket.aVoid();
-}
-else if (numder==5){
-    airTicket.aVoid();
-}
-else if (numder==6){
-    airTicket.aVoid();
-}
-else if (numder==7){
-    airTicket.aVoid();
-}
-else if (numder==8){
-    airTicket.aVoid();
-}
-else if (numder==9){
-    airTicket.aVoid();
-}
-else if (numder==10){
-    System.out.println("билетов нет");
-}
-else if (numder==0) {
-    System.out.println("Вы ошиблись номером");
-}
-else {
-    System.out.println("крабанкрок");
-}
+    public void transition() {
+        int numder = getRandomDiceNumber();
+        if (numder == 1) {
+            airTicket.aVoid();
+
+        } else if (numder == 2) {
+            airTicket.aVoid();
+        } else if (numder == 3) {
+            airTicket.aVoid();
+        } else if (numder == 4) {
+            airTicket.aVoid();
+        } else if (numder == 5) {
+            airTicket.aVoid();
+        } else if (numder == 6) {
+            airTicket.aVoid();
+        } else if (numder == 7) {
+            airTicket.aVoid();
+        } else if (numder == 8) {
+            airTicket.aVoid();
+        } else if (numder == 9) {
+            airTicket.aVoid();
+        } else if (numder == 10) {
+            System.out.println("билетов нет");
+        } else if (numder == 0) {
+            System.out.println("Вы ошиблись номером");
+        } else {
+            System.out.println("крабанкрок");
+        }
     }
-    public void tour(){
+
+    public void tour() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите страну");
         String country = scanner.nextLine();
